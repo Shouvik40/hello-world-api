e5b440c8a47561bee6cdf44af8e5befee32cc97d
Shouvik Paul <paulshouvik56@gmail.com>
2024-09-30 12:10:53 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
+//3000/
 app.listen(port, () => {
   console.log(`App is running at http://localhost:${port}`);
 });
